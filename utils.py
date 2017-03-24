e20f36098198f116022d8d4f72dd254d5bc8c61a
tbullmann <bullmann.torsten@gmail.com>
2017-03-24 12:29:05 +0900

    Stack sorted according to beam_index

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import os
+import re
 import pandas as pd
 import numpy as np
 from scipy.misc import imread
 from matplotlib import pyplot as plt
+from matplotlib import gridspec as gs
 from pyramid.decorator import reify
 
 EXAMPLE_HEXAGON = 'data/000046'
@@ -14,10 +16,17 @@ class ImageIterator:
         :param path: base path containing a file with coordinates and image filenames as well as images
         :param prefix:  'thumbnail' : display thumbnails
                         'image': display images
+        :return: self.tiles: dataframe with:
+            'filename': filename of the tile (image)
+            'x', 'y', 'z': coordinates of the lower left corner of the tiles
+            'beam_index': counterclockwise starting from the center (= third number in the filename - 1)
+            'file_index': order the tiles are listed in the coordinate file and read into the stack, starts from 0
         """
         self.path = path
         textfilename = os.path.join(path, prefix + '_coordinates.txt')
         self.tiles = pd.read_table(textfilename, names=['filename', 'x', 'y', 'z'])
+        self.tiles['beam_index'] = self.tiles['filename'].map(lambda x: int(re.findall(r'\d+', x)[2])-1)
+        self.tiles['file_index'] = self.tiles.index
         self.images = self.tiles.iterrows()
 
     def __iter__(self):
@@ -37,7 +46,7 @@ class ImageIterator:
         Plotting thumbnails.
         :param transform: image transformation; default None as well
         :param cmap: colormap; default: inverted gray scale
-        :param alpha: transparancy; default: 1 (not transparent)
+        :param alpha: transparency; default: 1 (not transparent)
         """
         for raw_image, tile in self:
             image = transform(raw_image) if transform else raw_image
@@ -48,19 +57,19 @@ class ImageIterator:
                        cmap=cmap,
                        alpha=alpha,
                        extent=[tile.x, tile.x+width, tile.y+height, tile.y])
-            plt.text(tile.x+width//2, tile.y+height//2, tile.name)
+            plt.text(tile.x+width//2, tile.y+height//2, tile.beam_index)
 
     @reify
     def stack(self):
-        """Loading all images into a stack"""
+        """Loading all images into a stack, sorted according to the beam_index"""
         image_list = []
         for raw_image, tile in self:
             image_list.append(raw_image)
-        return np.stack(image_list, axis=2)
+        return np.stack(image_list, axis=2)[:, :, np.argsort(self.tiles['beam_index'])]
 
     @reify
     def vignette(self):
-        return np.median(self.stack, axis=2)
+        return np.median(self.stack[:, :, 0:37], axis=2)
 
     @reify
     def offsets(self):
@@ -74,9 +83,8 @@ class ImageStack(ImageIterator):
         The 'focus artifact' produces the same vignette in all tiles, whereas the 'beam artifact' (maybe an
         'PMT artifact') is an offset for each tile.
         """
-        self.stack = self.stack \
-                     - self.vignette[:, :, None] + 127 \
-                     - self.offsets[None, None, :] + 127
+        self.stack = self.stack - self.offsets[None, None, :] + 127
+        self.stack = self.stack - self.vignette[:, :, None] + 127
         self.images = self.tiles.iterrows()   # Start new
         self.next = self.next_from_stack   # Use the stack for plotting
 
@@ -85,7 +93,7 @@ class ImageStack(ImageIterator):
         if tile is None:
             raise StopIteration()
         else:
-            raw_image = self.stack[:, :, index]
+            raw_image = self.stack[:, :, tile.beam_index]
             return raw_image, tile
 
 
@@ -93,13 +101,14 @@ def show_dataset():
     ImageIterator(EXAMPLE_HEXAGON).plot()
     # ImageIterator('data/000046', prefix='image').plot()  # slow
     plt.autoscale()
+    plt.axis('off')
     plt.show()
 
 
 def show_dataset_cleaned():
     data = ImageStack(EXAMPLE_HEXAGON, prefix='thumbnail')
     data.remove_focus_and_beam_artifact()
-    plt.imshow(data.vignette)
+    plt.imshow(data.vignette, cmap='gray_r')
     plt.show()
     data.plot()
     plt.autoscale()
@@ -110,7 +119,7 @@ def get_overlap():
     """Use afterimages produced by previous imaging the adjacent hexagons"""
     pass
 
-
 if __name__ == "__main__":
     show_dataset()
     show_dataset_cleaned()
+    get_overlap()
