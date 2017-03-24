4138a9e7aba1bc01749bf99d2fdb3f726d30dd00
tbullmann <bullmann.torsten@gmail.com>
2017-03-24 15:24:03 +0900

    Phase correlation

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy.misc import imread
 from matplotlib import pyplot as plt
 from matplotlib import gridspec as gs
+from skimage.feature import register_translation
+
 from pyramid.decorator import reify
 
 EXAMPLE_HEXAGON = 'data/000046'
@@ -38,7 +40,7 @@ class ImageIterator:
             raise StopIteration()
         else:
             fullfilename = os.path.join(self.path, tile.filename)
-            raw_image = imread(fullfilename, mode='L')
+            raw_image = imread(fullfilename, mode='L')   # Force gray scale
             return raw_image, tile
 
     def plot(self, transform=None, cmap='gray_r', alpha=1):
@@ -69,10 +71,12 @@ class ImageIterator:
 
     @reify
     def vignette(self):
+        """Median of center tiles only"""
         return np.median(self.stack[:, :, 0:37], axis=2)
 
     @reify
     def offsets(self):
+        """Median of each tile"""
         return np.median(self.stack, axis=(0, 1))
 
 
@@ -98,6 +102,7 @@ class ImageStack(ImageIterator):
 
 
 def show_dataset():
+    """Show single hexagon both as thumbnails and (full resolution) image"""
     ImageIterator(EXAMPLE_HEXAGON).plot()
     # ImageIterator('data/000046', prefix='image').plot()  # slow
     plt.autoscale()
@@ -106,6 +111,7 @@ def show_dataset():
 
 
 def show_dataset_cleaned():
+    """Show how to clean up the images"""
     data = ImageStack(EXAMPLE_HEXAGON, prefix='thumbnail')
     data.remove_focus_and_beam_artifact()
     plt.imshow(data.vignette, cmap='gray_r')
@@ -115,7 +121,7 @@ def show_dataset_cleaned():
     plt.show()
 
 
-def get_overlap():
+def get_shift_by_afterimage():
     """Use afterimages produced by previous imaging the adjacent hexagons"""
     data = ImageStack(EXAMPLE_HEXAGON, prefix='thumbnail')
     data.remove_focus_and_beam_artifact()
@@ -134,8 +140,44 @@ def get_overlap():
     ax.axis('off')
     plt.show()
 
+    # TODO Extract shift from edges, e.g. in the marginal
+
+
+def get_shift_by_phase_correlation(upsample_factor=1, alpha=0.5):
+    """Use phase correlation to identify the shift."""
+
+    # # Modified from: http://scikit-image.org/docs/dev/auto_examples/transform/plot_register_translation.html
+    # from skimage import data
+    # from scipy.ndimage import fourier_shift
+    # image = data.camera()
+    # shift = (-22.4, 13.32)
+    # # The shift corresponds to the pixel offset relative to the reference image
+    # offset_image = fourier_shift(np.fft.fftn(image), shift)
+    # offset_image = np.fft.ifftn(offset_image).real
+
+    data = ImageStack(EXAMPLE_HEXAGON, prefix='image')
+    data.remove_focus_and_beam_artifact()
+    image = data.stack[:, :, 0]
+    offset_image = data.stack[:, :, 1]
+
+    shift, error, diffphase = register_translation(image, offset_image, upsample_factor=upsample_factor)
+    print("Offset (y, x): {}".format(shift))
+
+    dy, dx = shift
+    height, width = image.shape[0:2]
+    plt.imshow(image, vmin=0, vmax=255, cmap='Blues_r', alpha=alpha,
+               extent=[0, width, height, 0])
+    plt.imshow(offset_image, vmin=0, vmax=255, cmap='Reds_r', alpha=alpha,
+               extent=[dx, dx+width, dy+height, dy])
+
+    plt.autoscale()
+    plt.show()
+
+    # TODO Test with images of adjacent hexagons (having a larger overlap, small overlap: FAIL)
+
 
 if __name__ == "__main__":
     show_dataset()
     show_dataset_cleaned()
-    get_overlap()
+    get_shift_by_afterimage()
+    get_shift_by_phase_correlation()
